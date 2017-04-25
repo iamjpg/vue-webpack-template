5a6ee3c0f5f37d2bff6c1e8ed11820af203eac7a
JP Given <jgiven@gmail.com>
2017-04-25 11:12:49 -0700

    Fixed readme and fixed the one unit test.

--- test/unit/specs/Hello.spec.js
+++ test/unit/specs/Hello.spec.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
-import Hello from '@/components/Hello'
+import FirstComponent from '@/components/FirstComponent'
 
-describe('Hello.vue', () => {
+describe('FirstComponent.vue', () => {
   it('should render correct contents', () => {
-    const Constructor = Vue.extend(Hello)
+    const Constructor = Vue.extend(FirstComponent)
     const vm = new Constructor().$mount()
-    expect(vm.$el.querySelector('.hello h1').textContent)
-      .to.equal('Welcome to Your Vue.js App')
+    expect(vm.$el.querySelector('p').textContent)
+      .to.equal('Data Output: This is the default message')
   })
 })
